808a138fff01a861a25017e60d5bbeb0cd519a10
callmejeje <choije401@naver.com>
2022-08-17 17:31:52 +0900

    feat: optional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const convertToDiscussion = (obj) => {
+  return (
+    <li>
+      <div>
+        <img src={obj.avatarUrl}></img>
+      </div>
+      <div>
+        <h2>
+          <a href={obj.url}>{obj.title}</a>
+        </h2>
+        <div>{`${obj.author} / ${obj.createdAt}`}</div>
+      </div>
+      <div>
+        <p>{obj.answer ? "☑" : "☒"}</p>
+      </div>
+    </li>
+  );
+};
 
 function App() {
+  const [list, setList] = useState();
+  useEffect(() => {
+    fetch("http://localhost:4000/discussions")
+      .then((response) => response.json())
+      .then((data) => setList(data.map(convertToDiscussion)));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Agora States</h1>
+      <section>
+        <div>
+          <span>Enter your name:</span>
+          <input></input>
+        </div>
+        <span>Enter your title:</span>
+        <input></input>
+        <div>Your question:</div>
+        <input placeholder="질문을 작성하세요"></input>
+        <button /*onClick={}*/>submit</button>
+      </section>
+      <section>
+        <ul>{list}</ul>
+      </section>
     </div>
   );
 }
